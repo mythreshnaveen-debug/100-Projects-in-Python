5d515ae9832ab4ffc76688be3c3c8fed026cf3a5
mythreshnaveen-debug <mythresh.naveen@gmail.com>
2025-11-28 23:13:32 -0600

    Update and rename Day1.py to BandNameGenerator.py

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,3 +1,7 @@
+# This is the first project I built
+# It creates a band name using your city, and the name of your pet.
+# Feel free to check it out!
+
 print("Welcome to the Band Name Generator")
 city = input("What's the name of the city you grew up in?\n")
 petName = input("What's your pet's name?\n")
