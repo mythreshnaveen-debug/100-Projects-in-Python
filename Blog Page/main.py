fe96eba5ca9f0dd6768d0a1a255cc8e5c1d559f2
mythreshnaveen-debug <mythresh.naveen@gmail.com>
2025-12-22 13:39:50 -0600

    Update main.py

--- Blog Page/main.py
+++ Blog Page/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-import requests, random
+import requests
 
 app = Flask(__name__)
 json = requests.get(url='https://api.npoint.io/c790b4d5cab58020d391').json()
 
 @app.route("/")
 def main():
-    return render_template('index.html', random_num=random, posts=json)
+    return render_template('index.html', posts=json)
 
 @app.route("/blogs/<int:blog_id>")
 def blog(blog_id):
