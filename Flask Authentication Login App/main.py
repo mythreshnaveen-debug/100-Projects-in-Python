d779fed1c0d0ef5adb88ed3e4871089abe55d599
Mythresh K. <mythresh.naveen@gmail.com>
2026-01-01 19:13:27 -0600

    Add files via upload

---
+++ Flask Authentication Login App/main.py
@@ -0,0 +1,104 @@
+import os
+from unicodedata import name
+
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+login_manager = LoginManager()
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+class User(db.Model, UserMixin):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = generate_password_hash(request.form["password"], method='pbkdf2:sha256', salt_length=8)
+        name = request.form["name"]
+        newUser = User(
+            email = email,
+            password = password,
+            name = name
+        )
+        db.session.add(newUser)
+        db.session.commit()
+
+        login_user(newUser)
+
+        return redirect(url_for('secrets'))
+
+
+    return render_template("register.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = db.session.query(User).filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('secrets'))
+            else:
+                flash("Password incorrect. Please try again.")
+                return redirect(url_for('login'))
+        else:
+            flash("That email does not exist. Please try again.")
+            return redirect(url_for('login'))
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+@login_required
+def secrets():
+    return render_template("secrets.html", name=current_user.name)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/download')
+def download():
+    return send_from_directory('static', path='files/cheat_sheet.pdf')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
